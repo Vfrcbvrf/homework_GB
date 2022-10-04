863f291c6d938e51ae61b7c81963409025a2e064
Maksim <agarkov05@inbox.ru>
2022-10-04 19:08:54 +0400

    все 4 задачки

---
+++ s1/Program.cs
@@ -0,0 +1,66 @@
+﻿
+
+//  задача 2
+
+// int a = int.Parse(Console.ReadLine()!);
+// int b = int.Parse(Console.ReadLine()!);
+
+// if (a>b){
+//     Console.WriteLine("max = " + a);
+// } else {
+//     Console.WriteLine("max = " + b);
+// }
+
+
+/////////////////////////////////////////////////////////////
+
+
+// задача 4
+
+// int fun(int r, int d){
+
+//     if (r>d) {
+//         return r;
+
+//     }else {
+//         return d;
+//     }
+// }
+
+// int a = int.Parse(Console.ReadLine()!);
+// int b = int.Parse(Console.ReadLine()!);
+// int c = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("max = " + fun(a, fun(b, c)));
+
+
+/////////////////////////////////////////////////////////////
+
+
+// задача 6
+
+// int a = int.Parse(Console.ReadLine()!);
+// if (a%2 == 0){
+//     Console.WriteLine("да");
+// }else Console.WriteLine("неа");
+
+
+/////////////////////////////////////////////////////////////
+
+
+// задача 8 
+
+int a = int.Parse(Console.ReadLine()!);
+for (int i= 1; i<=a; i++){
+
+    if (i%2 == 0){
+        Console.Write(i + " ");
+    }
+
+}
+
+
+
+
+
+
